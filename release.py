13252aa7bf793a198f8ca788ec5202c9df475986
root <root@shopper2.rwh.shop>
2020-01-11 09:23:32 -0500

    Added setup.py and renamed file: mtr.py to mtrrt...

---
+++ release.py
@@ -0,0 +1,88 @@
+#-----------------------------------------------------------------------------
+#   Copyright (c) 2020, networksecuritytoolkit.org. All rights reserved.
+#-----------------------------------------------------------------------------
+
+import mtraceroute
+
+name = 'mtraceroute'
+
+version = '1.30'
+
+description = 'A python3 library for performing an enhanced scapy Multi-Traceroute (MTR) with resulting SVG visual'
+
+keywords = [
+  'Networking', 'Systems Administration', 'Traceroute'
+]
+
+download_url = 'https://github.com/rwhalb/mtraceroute'
+
+author = 'Ronald W. Henderson'
+
+author_email = 'rwhalb@verizon.net'
+
+url = 'http://www.networksecuritytoolkit.org/'
+
+packages = [
+  'mtraceroute'
+]
+
+package_data = {
+  'mtraceroute': [
+  ],
+}
+
+install_requires = [
+  'scapy', 'netifaces', 'matplotlib'
+]
+
+license = 'GPLv2 License'
+
+long_description = """
+A python3 library for performing an enhanced scapy Multi-Traceroute (MTR)
+with resulting SVG visual.
+
+Features include running multiple queries with each target, display of
+Round Trip Time (RTT) calculations, selection of using
+Network Protocols: TCP, UDP and ICMP and with enhanced scapy
+SVG visual results and session packet capture output.
+
+The library is used by the Network Security Toolkit (NST WUI)
+that provides key enhancements including a GUI options interface,
+an interactive MTR SVG graphic, NST IPv4 Address Tools integration,
+IPv4 Address Geolocation, MTR session Packet Capture, ASN lookup,
+MTR historical session selection and management,
+MTR SVG graphic editing, MTR session console output access
+and SVG Graphic image conversion.
+
+See: http://wiki.networksecuritytoolkit.org/nstwiki/index.php/HowTo_Use_The_Scapy:_Multi-Traceroute_(MTR)
+"""
+
+platforms = 'OS Independent'
+
+scripts = ['mtrrt']
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Environment :: Console',
+  'Environment :: Plugins',
+  'Intended Audience :: Developers',
+  'Intended Audience :: Education',
+  'Intended Audience :: Information Technology',
+  'Intended Audience :: Science/Research',
+  'Intended Audience :: Network Administrators',
+  'Intended Audience :: Telecommunications Industry',
+  'License :: OSI Approved :: GPLv2 License',
+  'Natural Language :: English',
+  'Operating System :: OS Independent',
+  'Programming Language :: Python3',
+  'Topic :: Education :: Testing',
+  'Topic :: Internet',
+  'Topic :: Internet :: Log Analysis',
+  'Topic :: Software Development',
+  'Topic :: Software Development :: Libraries :: Python3 Modules',
+  'Topic :: System :: Networking',
+  'Topic :: System :: Operating System',
+  'Topic :: System :: Shells',
+  'Topic :: System :: Network Administration',
+  'Topic :: Utilities',
+]
---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+#-----------------------------------------------------------------------------
+#   A setup.py installation script modified for 'mtraceroute.py'.
+#-----------------------------------------------------------------------------
+"""
+A distutils Python setup file. For setuptools support see setup_egg.py.
+"""
+import os
+import sys
+
+from distutils.core import setup
+
+if os.path.exists('MANIFEST'):
+  os.remove('MANIFEST')
+
+import release
+#
+#-----------------------------------------------------------------------------
+def main():
+  if sys.version_info[:2] < (3, 6):
+    print("nstipgeolocate requires Python version 3.6.x or higher.")
+    sys.exit(1)
+
+  if sys.argv[-1] == 'setup.py':
+    print("To install, run 'python3 setup.py install'")
+    print()
+
+  setup(
+    name             = release.name,
+    version          = release.version,
+    description      = release.description,
+    keywords         = release.keywords,
+    download_url     = release.download_url,
+    author           = release.author,
+    author_email     = release.author_email,
+    url              = release.url,
+    packages         = release.packages,
+    package_data     = release.package_data,
+    license          = release.license,
+    long_description = release.long_description,
+    scripts          = release.scripts,
+    platforms        = release.platforms,
+    classifiers      = release.classifiers,
+    install_requires = release.install_requires,
+  )
+#
+#-----------------------------------------------------------------------------
+if __name__ == "__main__":
+  main()
