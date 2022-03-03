4bb0e245e9f317430b97b3938e8421d64c6cab99
Julien Manteau <julien.manteau@airbus.com>
2022-03-03 17:04:08 +0100

    Update

--- mtraceroute/__init__.py
+++ mtraceroute/__init__.py
@@ -9,15 +9,7 @@
 ##########################
 import ipaddress
 import re
-from scapy.base_classes import Net
-from scapy.config import conf
-from scapy.plist import SndRcvList
-from scapy.utils import incremental_label, colgen, do_graph
-from scapy.volatile import RandShort, RandInt, RandString, IncrementalValue
-from scapy.layers.inet import IP, TCP, UDP, ICMP
-from scapy.layers.inet6 import IPv6, ICMPv6TimeExceeded
-from scapy.packet import Raw
-from scapy.sendrecv import sr
+from scapy.all import *
 import time
 
 
